26ee2c640a14fe9ec3c3cbf8aeead10dbe22c35e
qixiaoye-oss <qixiaoye@gmail.com>
2025-12-19 10:19:41 -0500

    add code

--- pages/user/user/user.js
+++ pages/user/user/user.js
@@ -5,7 +5,7 @@ const pageLoading = require('../../../behaviors/pageLoading')
 Page({
   behaviors: [pageGuard.behavior, pageLoading],
   data: {
-    version: '1.4.37',
+    version: '1.0.2',
   },
   onShow: function () {
     this.startLoading()
